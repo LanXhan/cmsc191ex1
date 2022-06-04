1ecaf8e2a67d6a8cf8e0d843a59cff0359622d10
LanXhan <fltolentino1@up.edu.ph>
2022-06-04 19:34:26 +0800

    feat:edited script for SW algorithm

--- exer1.py
+++ exer1.py
@@ -5,12 +5,22 @@
 #or like sw.PNG
 
 def print_matrix1(a,x,y):
-	mrows = len(x)
-	ncols = len(y)
+	
+	newx='  '+x
+	newy='  '+y
+	mrows = len(newx)
+	ncols = len(newy)
 
+	print(newy)
 	for i in range(mrows):
 		for j in range(ncols):
-			print("%2d" % a[i][j], end=' ')
+			if (i==0):
+				print(newy[j], end='  ')
+			elif(j==0):
+				print(newx[i], end=' ')
+			else:
+			 	print("%2d" % a[i-1][j-1], end=' ')	
+			
 		print()
 
 
@@ -34,14 +44,82 @@ def gen_matrix(x, y, match_score=3, gap_cost=2):
 			insert = a[i][j - 1] - gap_cost
 			a[i][j]=max(match,delete,insert,0)
 
+
 	#print_matrix(a,x,y)	
 	return(a)
+
+# def traceback(a,x,y):
+# 	mrows = len(x)
+# 	ncols = len(y)
+# 	for i in range(mrows,0,-1):
+# 		for j in range(ncols,0,-1):
+# 				#print("indeice",i,j,end=' ')
+# 			if(a[i][j]!=0):
+# 				print("%2d" % a[i][j], end=' ')	
+			
+def traceback(score_matrix, start_pos,seq1,seq2): #adapted from https://gist.github.com/radaniba/11019717
+
+    END, DIAG, UP, LEFT = range(4)
+    aligned_seq1 = []
+    aligned_seq2 = []
+    x, y         = start_pos
+    move         = next_move(score_matrix, x, y) #depends the next move 
+    print(seq2,len(seq2))
+    print(seq1,len(seq1))
+    while move != END:
+        # print(seq1[x-1],seq2[y-1],move)
+        if move == DIAG:
+            if(seq1[x-1]==seq2[y-1]): #inroder to store the right sequence  if the seq1 matches sequence 2 it will write the corresponding symbol		
+                aligned_seq1.append(seq1[x - 1])
+                aligned_seq2.append(seq2[y - 1])
+            else: 			
+                aligned_seq1.append('')
+                aligned_seq2.append('')			
+            x -= 1
+            y -= 1
+        elif move == UP:
+            if(seq1[x-1]==seq2[y-1]):
+                aligned_seq1.append(seq1[x - 1])
+                aligned_seq2.append('')
+            else:			
+                aligned_seq1.append(seq1[x - 1])
+                aligned_seq2.append('-')
+            x -= 1
+        else:
+            aligned_seq1.append(' ')
+            aligned_seq2.append(seq2[y - 1])
+            y -= 1
+
+        move = next_move(score_matrix, x, y)
+
+    aligned_seq1.append(seq1[x - 1])
+    aligned_seq2.append(seq1[y - 1])
+
+    return ''.join(reversed(aligned_seq1)), ''.join(reversed(aligned_seq2))
+
+
+def next_move(score_matrix, x, y): # this dictates the next move dependeing on the score matrix 
+    diag = score_matrix[x - 1][y - 1]
+    up   = score_matrix[x - 1][y]
+    left = score_matrix[x][y - 1]
+    if diag >= up and diag >= left:     # Tie goes to the DIAG move.
+        return 1 if diag != 0 else 0    # 1 signals a DIAG move. 0 signals the end.
+    elif up > diag and up >= left:      # Tie goes to UP move.
+        return 2 if up != 0 else 0      # UP move or end.
+    elif left > diag and left > up:
+        return 3 if left != 0 else 0    # LEFT move or end.
+    else:
+        # Execution should not reach here.
+        raise ValueError('invalid move during traceback')
+			
 	
+
 x = "GGTTGACTA"	
 y = "TGTTACGG"
 
 a=gen_matrix(x,y)
 
 print_matrix1(a,x,y)
-
-
+res=traceback(a,(len(x),len(y)),x,y)
+print(res[0],end='\n')
+print(res[1],end='\n')
